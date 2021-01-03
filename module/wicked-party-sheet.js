a7c1868f5e370fd028d0111c381585245f7ed634
Eike Hanus <eike.hanus@t-online.de>
2021-01-03 17:23:45 +0100

    Added heart-slashing logic for the adventure party sheet

--- module/wicked-party-sheet.js
+++ module/wicked-party-sheet.js
@@ -23,25 +23,74 @@ export class WickedPartySheet extends WickedSheet {
   getData() {
     const data = super.getData();
 
-
     // Override Code for updating the sheet goes here
-    if (true) {
-      let a = 1;
-      data.items.forEach(e => {
-        if (e.type == "adventurer") {
-          e.data.hearts = [];
-          for (var i = 1; i < 6; i++) {
-            if (e.data.tier + 1 >= i) {
-              e.data.hearts[i] = ""
+    let max_tier = 0;
+    data.items.forEach(e => {
+      if (e.type == "adventurer" && e.data.adventurer_type == "adventurer") {
+        if ((e.data.tier) > max_tier) {
+          max_tier = e.data.tier;
+        }
+      }
+    });
+
+    data.items.forEach(e => {
+      if (e.type == "adventurer" && e.data.adventurer_type == "adventurer") {
+        e.data.hearts = [];
+        let slashes_left = e.data.heart_slashes ?? 0;
+        for (var i = 5; i > 0; i--) {
+          if (i > max_tier + 1) {
+            e.data.hearts[i] = "hidden";
+          } else if (i > e.data.tier + 1 ) {
+            e.data.hearts[i] = "greyed";
+          } else {
+            // Distribute slashes
+            if (slashes_left > 1) {
+              e.data.hearts[i] = "slashed-2";
+              slashes_left -= 2;
+            } else if (slashes_left == 1) {
+              e.data.hearts[i] = "slashed-1";
+              slashes_left = 0;
             } else {
-              e.data.hearts[i] = "greyed"
+              e.data.hearts[i] = "";
             }
           }
         }
-      });
-    }
+      }
+    });
+
 
     return data;
+  }
+
+  /* -------------------------------------------- */
+
+  async _onHeartSetSlashes(event) {
+
+    if (event.currentTarget.classList.contains("greyed")) {
+      return;
+    }
+
+    let selected_heart = event.currentTarget.control.value;
+    const itemId = event.currentTarget.closest(".item").dataset.itemId;
+    const item = this.actor.getOwnedItem(itemId);
+
+    const hearts = item.data.data.tier + 1;
+    const slashes = item.data.data.heart_slashes ?? 0;
+    let new_slashes = slashes;
+
+    if (event.currentTarget.classList.contains("slashed-2")) {
+      // Remove slashes from current heart and all hearts to the left
+      new_slashes = (hearts - selected_heart) * 2;
+    } else if (event.currentTarget.classList.contains("slashed-1")) {
+      // Set current heart to slashed 2
+      new_slashes = (hearts - selected_heart + 1) * 2;
+    } else {
+      // Set current heart to slashed 1 and all to the right to slashed 2
+      new_slashes = (hearts - selected_heart) * 2 + 1;
+    }
+
+    // Update Data
+    item.update({ ['data.heart_slashes']: new_slashes });
   }
 
   /* -------------------------------------------- */
@@ -71,6 +120,9 @@ export class WickedPartySheet extends WickedSheet {
     html.find('.add-item').click(ev => {
       WickedHelpers._addOwnedItem(ev, this.actor);
     });
+
+    // Update Hearts
+    html.find('#adventure-party .adv-heart').click(this._onHeartSetSlashes.bind(this));
 
   }
 
