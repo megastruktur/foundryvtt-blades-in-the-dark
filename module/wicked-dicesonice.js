6aed1737dd1ffc7d72969f9bc11e7fbe0ee44c8b
LorduFreeman <matrixretoasted@gmx.net>
2021-01-12 16:47:05 +0100

    Added white dot dice style

--- module/wicked-dicesonice.js
+++ module/wicked-dicesonice.js
@@ -1,7 +1,29 @@
 
 Hooks.on('diceSoNiceReady', (dice3d) => {
-  dice3d.addSystem({ id: "Wicked Dots", name: "Wicked Dots"},"exclusive");
+  dice3d.addSystem({ id: "WhiteDots", name: "Wicked Dots"},"exclusive");
+   dice3d.addDicePreset({
+	   type: "d6",
+	   labels: [
+       "../systems/wicked-ones/styles/assets/dice/whitedots/d6-1.png",
+       "../systems/wicked-ones/styles/assets/dice/whitedots/d6-2.png",
+       "../systems/wicked-ones/styles/assets/dice/whitedots/d6-3.png",
+       "../systems/wicked-ones/styles/assets/dice/whitedots/d6-4.png",
+       "../systems/wicked-ones/styles/assets/dice/whitedots/d6-5.png",
+       "../systems/wicked-ones/styles/assets/dice/whitedots/d6-6.png"	   
+	   ],
+	   bumpMaps: [
+       "../systems/wicked-ones/styles/assets/dice/whitedots/d6-1-b.png",
+       "../systems/wicked-ones/styles/assets/dice/whitedots/d6-2-b.png",
+       "../systems/wicked-ones/styles/assets/dice/whitedots/d6-3-b.png",
+       "../systems/wicked-ones/styles/assets/dice/whitedots/d6-4-b.png",
+       "../systems/wicked-ones/styles/assets/dice/whitedots/d6-5-b.png",
+       "../systems/wicked-ones/styles/assets/dice/whitedots/d6-6-b.png"	  
+		],
+		system: "WhiteDots",
+		colorset:"Wicked Dice"
+   },"d6");
 
+  dice3d.addSystem({ id: "BlackDots", name: "Wicked Dark Dots"},"exclusive");
    dice3d.addDicePreset({
 	   type: "d6",
 	   labels: [
@@ -20,11 +42,11 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
        "../systems/wicked-ones/styles/assets/dice/dots/d6-5-b.png",
        "../systems/wicked-ones/styles/assets/dice/dots/d6-6-b.png"	  
 		],
-		system: "Wicked Dots",
+		system: "BlackDots",
 		colorset:"Wicked Dice"
    },"d6");
 
-  dice3d.addSystem({ id: "Wicked Claws", name: "Wicked Claws"},"exclusive");
+  dice3d.addSystem({ id: "Claws", name: "Wicked Claws"},"exclusive");
 
    dice3d.addDicePreset({
 	   type: "d6",
@@ -44,12 +66,28 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
        "../systems/wicked-ones/styles/assets/dice/claws/d6-5-b.png",
        "../systems/wicked-ones/styles/assets/dice/claws/d6-6-b.png"	  
 		],
-		system: "Wicked Claws"
+		system: "Claws",
+		colorset:"Wicked Dice"
    },"d6");   
+
+   dice3d.addSystem({ id: "Numbers", name: "Wicked Numbers"},"exclusive");
+   dice3d.addDicePreset({
+	   type: "d6",
+	   labels: [
+       "1",
+       "2",
+       "3",
+       "4",
+       "5",
+       "6"	   
+	   ],
+	   	system: "Numbers",
+	   	colorset:"Wicked Dice"
+   },"d6");  
 
       dice3d.addColorset({
         name: 'Wicked Dice',
-        description: "Wicked Ones Style",
+        description: "Wicked Style",
         category: "Wicked Ones",
 		foreground: "#750000",
 		background: "#d1d1d1",
