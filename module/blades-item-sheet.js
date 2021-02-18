bac5dde121495a13d03430d1b4105fde8e34c80f
jonasPri <jonasaberg@outlook.com>
2021-02-18 14:28:33 +0100

    gm section and notes

--- module/blades-item-sheet.js
+++ module/blades-item-sheet.js
@@ -45,7 +45,11 @@ export class BladesItemSheet extends ItemSheet {
   /** @override */
 getData() {
   const data = super.getData();
+  console.log(game);
+  console.log(game.user);
+  console.log(game.user.isGM);
   data.isGm = game.user.isGM;
+  sheetData["isGM"] = game.user.isGM;
   return data;
 }
 }
