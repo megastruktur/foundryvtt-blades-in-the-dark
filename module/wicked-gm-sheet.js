3dca35f0d04b08d1a88700e228193ba67e9c1932
Eike Hanus <eike.hanus@t-online.de>
2021-02-19 17:14:34 +0100

    Updated phase hint system and added remaining hints

--- module/wicked-gm-sheet.js
+++ module/wicked-gm-sheet.js
@@ -23,14 +23,25 @@ export class WickedGMSheet extends WickedSheet {
   getData() {
     const data = super.getData();
 
-    // look for abilities that change the number of gold, supply and dark heart icons
-    // also check for Doomseeker rays and add translations
+    // Progressivly count up the invasion items
     let invasion_count = 1;
     data.items.forEach(i => {
       if (i.type == "invasion") {
         i.data.inv_number = invasion_count++;
       }
     });
+
+    // Add a hint for the selected phase and scaffold HTML
+    data.data.phase_hint = "<ul>";
+    for (var i = 1; i < 10; i++) {
+      let new_hint = "FITD.CYCLE.PHASE" + data.data.current_phase.value + ".Hint" + i;
+      let new_hint_loc = game.i18n.localize(new_hint);
+      if (new_hint == new_hint_loc) {
+        break;
+      }
+      data.data.phase_hint += "<li>" + new_hint_loc + "</li>"
+    }
+    data.data.phase_hint += "</ul>";
 
     return data;
   }
