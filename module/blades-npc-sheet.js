5cc5c51eeb33fc1c84c52623bb79c521265a78a8
Justin Ross <justin@thsjst.in>
2021-05-07 11:15:45 -0700

    Added an "npc" actor type and added a compendium of NPCs.

---
+++ module/blades-npc-sheet.js
@@ -0,0 +1,44 @@
+
+import { BladesSheet } from "./blades-sheet.js";
+
+/**
+ * @extends {BladesSheet}
+ */
+export class BladesNPCSheet extends BladesSheet {
+
+  /** @override */
+	static get defaultOptions() {
+	  return mergeObject(super.defaultOptions, {
+  	  classes: ["blades-in-the-dark", "sheet", "actor"],
+  	  template: "systems/blades-in-the-dark/templates/npc-sheet.html",
+      width: 900,
+      height: 'auto',
+      tabs: [{navSelector: ".tabs", contentSelector: ".tab-content"}]
+    });
+  }
+  
+  
+  
+  /** @override */
+	activateListeners(html) {
+    super.activateListeners(html);
+
+    // Everything below here is only needed if the sheet is editable
+    if (!this.options.editable) return;
+
+    // Update Inventory Item
+    // html.find('.item-body').click(ev => {
+    //   const element = $(ev.currentTarget).parents(".item");
+    //   const item = this.actor.getOwnedItem(element.data("itemId"));
+    //   item.sheet.render(true);
+    // });
+	
+    // // Delete Inventory Item
+    // html.find('.item-delete').click(ev => {
+    //   const element = $(ev.currentTarget).parents(".item");
+    //   this.actor.deleteOwnedItem(element.data("itemId"));
+    //   element.slideUp(200, () => this.render(false));
+    // });
+	
+	}
+}
