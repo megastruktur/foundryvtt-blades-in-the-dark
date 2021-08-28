35035b08b158f0740573b9f6382651a7f07ca9b2
conordjpc <conor@conordjpc.com>
2021-08-29 00:25:40 +0100

    Added Vehicle & squad naming & fixed compendium
    added the vehicle to template, the vehicle statuses to the character and changed some squad names
    fixed the compendium. Also added the Vehicle damage table to actor sheet

--- module/blades.js
+++ module/blades.js
@@ -47,7 +47,7 @@ Hooks.once("init", async function() {
   Items.unregisterSheet("core", ItemSheet);
   Items.registerSheet("blades", BladesItemSheet, {makeDefault: true});
   await preloadHandlebarsTemplates();
-  
+
   Actors.registeredSheets.forEach(element => console.log(element.Actor.name));
 
 
@@ -271,7 +271,7 @@ Hooks.once("ready", function() {
 
   // Determine whether a system migration is required
   const currentVersion = game.settings.get("bitd", "systemMigrationVersion");
-  const NEEDS_MIGRATION_VERSION = 2.15;
+  const NEEDS_MIGRATION_VERSION = 0.1.3;
 
   let needMigration = (currentVersion < NEEDS_MIGRATION_VERSION) || (currentVersion === null);
 
