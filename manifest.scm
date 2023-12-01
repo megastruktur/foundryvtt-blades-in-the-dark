409f00195f0dbc226baf1e7def9d358faca1e60b
TakeV <takev@disroot.org>
2023-12-01 13:05:16 -0500

    Add guix manifest

---
+++ manifest.scm
@@ -0,0 +1,5 @@
+;; What follows is a "manifest" equivalent to the command line you gave.
+;; You can store it in a file that you may then pass to any 'guix' command
+;; that accepts a '--manifest' (or '-m') option.
+
+(specifications->manifest (list "node"))
